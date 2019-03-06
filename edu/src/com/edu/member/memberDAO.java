ecf5744126d0e7b6a3a01f832f189ce7fadb23b2
ghd075 <rlfls54@gmail.com>
2019-03-06 17:43:07 +0900

    Add memberDAO.java & memberDTO.java

---
+++ edu/src/com/edu/member/memberDAO.java
@@ -0,0 +1,171 @@
+package com.edu.member;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.edu.common.ConnectionManager;
+
+public class memberDAO {
+	Connection conn = null;
+	PreparedStatement pstmt = null;
+	ResultSet rs = null;
+	
+	// 싱글톤 : static 필드
+	static memberDAO instance;
+
+	public static memberDAO getInstance() {
+		if (instance == null)
+			instance = new memberDAO();
+		return instance;
+	}
+	
+	//등록
+	public int insert(memberDTO dto) {
+		int r = 0;
+		try {
+			//1. DB연결
+			conn = ConnectionManager.getConnnect();
+			//트랜잭션 처리
+			conn.setAutoCommit(false);
+			//2. sql구문 준비(삽입)
+			String sql = "insert into member (userid, userpw, username, "
+					+ "gender, regdate) values (?,?,?,?,?)";
+			pstmt = conn.prepareStatement(sql);
+			//3. 실행
+			pstmt.setString(1, dto.getUserid());
+			pstmt.setString(2, dto.getUserpw());
+			pstmt.setString(3, dto.getUserpw());
+			pstmt.setString(4, dto.getGender());
+			pstmt.setDate(5, dto.getRegdate());;
+			r = pstmt.executeUpdate();
+			conn.commit();
+			//4. 결과처리
+			System.out.println(r + " 건이 등록됨");
+		}catch (SQLException e) {
+			System.out.println("등록실패");
+			e.printStackTrace();
+			try {
+				conn.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+		}finally {
+			//5. 연결해제
+			ConnectionManager.close(conn);
+		}
+		return r;
+	}
+	//수정
+	public int update(memberDTO dto) {
+		int r = 0;
+		
+		try {
+			//1. DB연결
+			conn = ConnectionManager.getConnnect();
+			//2. sql구문 준비(수정)
+			String sql = "update member set job = ?, hobby = ?, "
+					+ "info = ? where userid = ?";
+			pstmt = conn.prepareStatement(sql);
+			//3. 실행
+			pstmt.setString(1, dto.getJob());
+			pstmt.setString(2, dto.getHobby());
+			pstmt.setString(3, dto.getInfo());
+			pstmt.setString(4, dto.getUserid());
+			r = pstmt.executeUpdate();
+			//4. 결과처리
+			System.out.println(r + " 건이 수정됨");
+		}catch (SQLException e) {
+			System.out.println("등록실패");
+			e.printStackTrace();
+		}finally {
+			//5. 연결해제
+			ConnectionManager.close(conn);
+		}
+		return r;
+	}
+	//삭제
+	public int delete(memberDTO dto) {
+		int r = 0;
+		
+		try {
+			//1. DB연결
+			conn = ConnectionManager.getConnnect();
+			//2. sql구문 준비(삭제)
+			String sql = "delete member where userid = ?";
+			pstmt = conn.prepareStatement(sql);
+			//3. 실행
+			pstmt.setString(1, dto.getUserid());
+			r = pstmt.executeUpdate();
+			//4. 결과처리
+			System.out.println(r + " 건이 삭제됨");
+		}catch (SQLException e) {
+			System.out.println("등록실패");
+			e.printStackTrace();
+		}finally {
+			//5. 연결해제
+			ConnectionManager.close(conn);
+		}
+		return r;
+	}
+	//단건조회
+	public memberDTO selectOne(memberDTO dto) {
+		memberDTO member = new memberDTO();
+		try {
+			conn = ConnectionManager.getConnnect();
+			String sql = "select * from member where userid = ?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, dto.getUserid());
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				member.setUserid(rs.getString("userid"));
+				member.setUserpw(rs.getString("userpw"));
+				member.setUsername(rs.getString("username"));
+				member.setJob(rs.getString("job"));
+				member.setHobby(rs.getString("hobby"));
+				member.setInfo(rs.getString("if"));
+				member.setGender(rs.getString("gender"));
+				member.setRegdate(rs.getDate("regdate"));
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			//연결해제
+			ConnectionManager.close(conn);
+		}
+		return member;
+	}
+
+	// 전체조회
+	public List<memberDTO> selectAll() {
+		List<memberDTO> datas = new ArrayList<memberDTO>();
+		try {
+			conn = ConnectionManager.getConnnect();
+			String sql = "select * from member order by userid";
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				memberDTO member = new memberDTO();
+				member.setUserid(rs.getString("userid"));
+				member.setUserpw(rs.getString("userpw"));
+				member.setUsername(rs.getString("username"));
+				member.setJob(rs.getString("job"));
+				member.setHobby(rs.getString("hobby"));
+				member.setInfo(rs.getString("if"));
+				member.setGender(rs.getString("gender"));
+				member.setRegdate(rs.getDate("regdate"));
+				datas.add(member); // dto를 리스트에 추가
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// 연결해제
+			ConnectionManager.close(conn);
+		}
+		return datas;
+	}
+}
---
+++ edu/src/com/edu/member/memberDTO.java
@@ -0,0 +1,78 @@
+package com.edu.member;
+
+import java.sql.*;
+
+public class memberDTO {
+	private String userid;     
+	private String userpw;     
+	private String username; 
+	private String job;                    
+	private String hobby;            
+	private String info;                      
+	private String gender;         
+	private Date regdate;
+	
+	public String getUserid() {
+		return userid;
+	}
+	
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	
+	public String getUserpw() {
+		return userpw;
+	}
+	
+	public void setUserpw(String userpw) {
+		this.userpw = userpw;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getJob() {
+		return job;
+	}
+	
+	public void setJob(String job) {
+		this.job = job;
+	}
+	
+	public String getHobby() {
+		return hobby;
+	}
+	
+	public void setHobby(String hobby) {
+		this.hobby = hobby;
+	}
+	
+	public String getInfo() {
+		return info;
+	}
+	
+	public void setInfo(String info) {
+		this.info = info;
+	}
+	
+	public String getGender() {
+		return gender;
+	}
+	
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+	public Date getRegdate() {
+		return regdate;
+	}
+	
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+}
