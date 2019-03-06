d2054487c99b868e8bd85944db880f6b9c5106bf
ghd075 <rlfls54@gmail.com>
2019-03-06 16:15:14 +0900

    Modify DeptDAO.java[department_id]

--- edu/src/com/edu/dept/DeptDAO.java
+++ edu/src/com/edu/dept/DeptDAO.java
@@ -4,12 +4,14 @@ import java.sql.*;
 import java.util.*;
 
 import com.edu.common.ConnectionManager;
+import com.edu.emp.EmpDTO;
 
 public class DeptDAO {
 	//필드선언
 	private Connection conn = null;	
 	private PreparedStatement pstmt = null;
 	private ResultSet rs = null;
+	
 	//싱글
 	static DeptDAO instance;
 	public static DeptDAO getInstance() {
@@ -39,5 +41,30 @@ public class DeptDAO {
 			ConnectionManager.close(conn);
 		}
 		return dept;
+	}
+	
+	//전체조회
+	public List<DeptDTO> selectAll(){
+		List<DeptDTO> datas = new ArrayList<DeptDTO>();
+		try {
+			conn = ConnectionManager.getConnnect();
+			String sql = "select * from departments order by department_id";
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			while(rs.next()) {
+				DeptDTO dept = new DeptDTO();
+				dept.setDepartment_id(rs.getString("department_id"));
+				dept.setDepartment_name(rs.getString("department_name"));
+				dept.setManager_id(rs.getString("manager_id"));
+				dept.setLocation_id(rs.getString("location_id"));
+				datas.add(dept); //dto를 리스트에 추가
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			//연결해제
+			ConnectionManager.close(conn);
+		}
+		return datas; 
 	}
 }
