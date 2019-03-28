66d44f1fbce042504f34b9bb48c3470f066b7186
ghd075 <rlfls54@gmail.com>
2019-03-28 09:53:37 +0900

    Modify eventBasic.js[중복 정의시 이벤트핸들러 적용]

--- javascript/WebContent/js/eventBasic.js
+++ javascript/WebContent/js/eventBasic.js
@@ -1,7 +1,12 @@
 function doProcess() {
     alert('event attr:\n이벤트 발생');
 }
-//페이지가 로딩될 때 실행해라
+function doProcess2() {
+    alert('event attr:\n이벤트 발생2');
+}
 window.onload = function(){
     document.getElementById("btn1").onclick = doProcess;
+    document.getElementById("btn1").onclick = doProcess2;
 }
+//중복으로 정의하면 마지막으로 추가할 이벤트로 덮어씀
+//페이지가 로딩될 때 실행해라
