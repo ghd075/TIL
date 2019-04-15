b28a64cb66c9602fea7e26b83ae4cb9b65bfb5e7
ghd075 <rlfls54@gmail.com>
2019-04-15 16:20:57 +0900

    어노테이션 기반 AOP[Before 어드바이스]

--- BoardWeb2/src/main/java/com/spirngbook/biz/common/BeforeAdvice.java
+++ BoardWeb2/src/main/java/com/spirngbook/biz/common/BeforeAdvice.java
@@ -1,9 +1,22 @@
 package com.spirngbook.biz.common;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Service;
 
+//빈 등록
+@Service
 //Advice 클래스
+@Aspect //Aspect = Pointcut + Advice
 public class BeforeAdvice {
+	//포인트 컷 설정
+	@Pointcut("execution(* com.spirngbook.biz..*Impl.*(..))")
+	public void allPointcut() {} //포인트 컷 이름 설정
+	
+	//어드바이스 설정
+	@Before("allPointcut()") //포인트 컷 연결 및 동작시점 지정 
 	public void beforeLog(JoinPoint jp) {
 		String methodName = jp.getSignature().getName();
 		Object[] args = jp.getArgs();
