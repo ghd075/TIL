b0f7520dd2c80f338f5e4e69f99cbb4b5d0838f7
ghd075 <rlfls54@gmail.com>
2019-04-15 16:04:00 +0900

    JoinPoint를 이용한 Around 어드바이스 구현

--- BoardWeb2/src/main/java/com/spirngbook/biz/common/AroundAdvice.java
+++ BoardWeb2/src/main/java/com/spirngbook/biz/common/AroundAdvice.java
@@ -1,12 +1,25 @@
 package com.spirngbook.biz.common;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.springframework.util.StopWatch;
 
 public class AroundAdvice {
 	public Object aroundLog(ProceedingJoinPoint pjp) throws Throwable {
-		System.out.println("[BEFORE]: 비즈니스 메소드 수행 전에 처리할 내용...");
-		Object obj = pjp.proceed(); //포인트컷(서비스 메서드) 호출 -> 실행
-		System.out.println("[AFTER]: 비즈니스 메소드 수행 후에 처리할 내용...");
+		String methodName = pjp.getSignature().getName();
+		StopWatch stopWatch = new StopWatch();
+		stopWatch.start();
+		
+		Object obj = pjp.proceed(); //포인트컷(서비스 메서드) 호출
+		
+		stopWatch.stop();
+		
+		System.out.println("[around] "  +  methodName + " : "  + stopWatch.getTotalTimeMillis());
+		
+		/*
+		 * System.out.println("[BEFORE]: 비즈니스 메소드 수행 전에 처리할 내용..."); Object obj =
+		 * pjp.proceed(); //포인트컷(서비스 메서드) 호출 -> 실행
+		 * System.out.println("[AFTER]: 비즈니스 메소드 수행 후에 처리할 내용...");
+		 */
 		return obj;
 	}
 }
