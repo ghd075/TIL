607b3ab4175b9fabbbd87c0015bbcce035b128b4
ghd075 <rlfls54@gmail.com>
2019-04-15 16:31:40 +0900

    어노테이션 기반 AOP[Around 어드바이스 설정!!]

--- BoardWeb2/src/main/java/com/spirngbook/biz/common/AroundAdvice.java
+++ BoardWeb2/src/main/java/com/spirngbook/biz/common/AroundAdvice.java
@@ -1,9 +1,15 @@
 package com.spirngbook.biz.common;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Service;
 import org.springframework.util.StopWatch;
 
+@Service
+@Aspect
 public class AroundAdvice {
+	@Around("BeforeAdvice.allPointcut()")
 	public Object aroundLog(ProceedingJoinPoint pjp) throws Throwable {
 		String methodName = pjp.getSignature().getName();
 		StopWatch stopWatch = new StopWatch();
