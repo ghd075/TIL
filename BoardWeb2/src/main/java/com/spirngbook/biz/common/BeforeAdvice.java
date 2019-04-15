983c8e0c36adc2d2792d1dd79b62941989e7663a
ghd075 <rlfls54@gmail.com>
2019-04-15 15:27:11 +0900

    JoinPoint를 이용해서 Before 어드바이스 구현

--- BoardWeb2/src/main/java/com/spirngbook/biz/common/BeforeAdvice.java
+++ BoardWeb2/src/main/java/com/spirngbook/biz/common/BeforeAdvice.java
@@ -1,7 +1,13 @@
 package com.spirngbook.biz.common;
 
+import org.aspectj.lang.JoinPoint;
+
+//Advice 클래스
 public class BeforeAdvice {
-	public void beforeLog() {
-		System.out.println("[사전 처리] 비즈니스 로직 수행 전 동작");
+	public void beforeLog(JoinPoint jp) {
+		String methodName = jp.getSignature().getName();
+		Object[] args = jp.getArgs();
+		System.out.println("[사전 처리] " + methodName +"비즈니스 로직 수행 전 동작"
+							+ ((args != null && args.length>0) ? args[0] : ""));
 	}
 }
