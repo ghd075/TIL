c951122c636ecf487755b5a8438568b2a8f6e6ac
ghd075 <rlfls54@gmail.com>
2019-04-15 16:25:14 +0900

    어노테이션 기반 AOP[After Returning 어드바이스]

--- BoardWeb2/src/main/java/com/spirngbook/biz/common/AfterReturningAdvice.java
+++ BoardWeb2/src/main/java/com/spirngbook/biz/common/AfterReturningAdvice.java
@@ -1,9 +1,15 @@
 package com.spirngbook.biz.common;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Service;
 
-
+@Service
+@Aspect
 public class AfterReturningAdvice {
+	
+	@AfterReturning(pointcut = "BeforeAdvice.allPointcut()", returning = "returnObj")
 	public void afterLog(JoinPoint jp, Object returnObj) {
 		String methodName = jp.getSignature().getName();
 		System.out.println("[사후 처리]" + methodName + " : " + (returnObj != null ? returnObj : ""));
