d236750c0cf529a71cf7150b10eabd4f82c9a9c9
ghd075 <rlfls54@gmail.com>
2019-04-16 16:14:02 +0900

    @Before와 @BeforeClass 차이 비교!!

--- BoardWeb2/src/test/java/com/spirngbook/biz/board/BoardClientJunit.java
+++ BoardWeb2/src/test/java/com/spirngbook/biz/board/BoardClientJunit.java
@@ -1,27 +1,47 @@
 package com.spirngbook.biz.board;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+//import static org.junit.Assert.fail;
 
+import org.junit.Before;
+//import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.spirngbook.biz.board.impl.BoardDAOSpring;
+//import com.spirngbook.biz.board.impl.BoardDAOSpring;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations="classpath:applicationContext.xml")
 public class BoardClientJunit {
 	
 	@Autowired
-	BoardDAOSpring dao;
+	BoardService service;
+	
+	//static BoardVO vo;
+	BoardVO vo;
+	
+	//최초 한 번만 실행할 수 있도록 설정
+//	@BeforeClass
+	@Before
+	public void init() {
+		vo = new BoardVO();
+	}
+	/*
+	 * public static void init() { vo = new BoardVO(); }
+	 */
 	
 	@Test
 	public void test() {
-		BoardVO vo = new BoardVO();
 		vo.setSeq(1);
-		assertEquals(dao.getBoard(vo).getWriter(), "관리자");
+		assertEquals(service.getBoard(vo).getWriter(), "관리자");
 //		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void deleteTest() {
+		vo.setSeq(8);
+		service.deleteBoard(vo);
 	}
 }
