205bfa6e7e493aded8c1d28356327eebd88318d9
ghd075 <rlfls54@gmail.com>
2019-04-16 15:21:38 +0900

    JUnit을 사용하기

--- BoardWeb2/src/test/java/com/spirngbook/biz/board/BoardClientJunit.java
+++ BoardWeb2/src/test/java/com/spirngbook/biz/board/BoardClientJunit.java
@@ -3,7 +3,11 @@ package com.spirngbook.biz.board;
 import static org.junit.Assert.fail;
 
 import org.junit.Test;
-
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
 public class BoardClientJunit {
 	
 	@Test
