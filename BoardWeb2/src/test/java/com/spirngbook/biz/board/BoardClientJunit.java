77820a9b70779acf3072f23509c9b52673642156
ghd075 <rlfls54@gmail.com>
2019-04-16 15:30:10 +0900

    JUnit을 이용한 단건 조회!!

--- BoardWeb2/src/test/java/com/spirngbook/biz/board/BoardClientJunit.java
+++ BoardWeb2/src/test/java/com/spirngbook/biz/board/BoardClientJunit.java
@@ -1,17 +1,27 @@
 package com.spirngbook.biz.board;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.spirngbook.biz.board.impl.BoardDAOSpring;
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration
+@ContextConfiguration(locations="classpath:applicationContext.xml")
 public class BoardClientJunit {
+	
+	@Autowired
+	BoardDAOSpring dao;
 	
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		BoardVO vo = new BoardVO();
+		vo.setSeq(1);
+		assertEquals(dao.getBoard(vo).getWriter(), "관리자");
+//		fail("Not yet implemented");
 	}
 }
